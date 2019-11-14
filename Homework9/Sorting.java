34a1a03882bdd54a5148f071d38385531fa19c68
Naeem Mannan <nmannan97@gmail.com>
2019-11-13 19:40:29 -0800

    Updates

--- Homework9/Sorting.java
+++ Homework9/Sorting.java
@@ -34,41 +34,6 @@ public class Sorting
         return Return;
     }
 
-    public static int[] mergeSort(int[] list)
-    {
-        int[] Return = new int[32];
-        Return = list;
-        ArrayList arr = new ArrayList();
-        int temp = 0;
-        int count = 0;
-        int length = list.length;
-        int min = 0;
-        int start = 0;
-        int end = 0;
-        while(length>0){
-            count+= 1;
-            length = length/2;
-        }
-        for(int i = 0;i<count;i++)
-        {
-            min = list[i];
-            end = (int)Math.pow(2,i);
-            for(int k = 0;k<end && k<list.length;k++)
-            {
-                if(list[k]<min)
-                {
-                    temp = min;
-                    min = list[k];
-                    list[k] = temp;
-                } 
-                Return[k] = min;
-
-            }
-            end = 0;
-        }
-        return Return;
-    }
-
     public static void quickSort(int[] list) {   
         quickSort(list, 0, list.length - 1);  
     }  
@@ -80,6 +45,33 @@ public class Sorting
             quickSort(list, first, pivotIndex - 1);    
             quickSort(list, pivotIndex + 1, last); 
         }  
+    }
+
+    public static void mergeSort(int[] list) {    
+        if (list.length > 1) {      // Merge sort the first half  
+            int[] firstHalf = new int[list.length / 2];    
+            System.arraycopy(list, 0, firstHalf, 0, list.length / 2);
+            mergeSort(firstHalf);      // Merge sort the second half     
+            int secondHalfLength = list.length - list.length / 2;     
+            int[] secondHalf = new int[secondHalfLength];      
+            System.arraycopy(list, list.length / 2,        secondHalf, 0, secondHalfLength);
+            mergeSort(secondHalf);      // Merge firstHalf with secondHalf into list 
+            merge(firstHalf, secondHalf, list);    }  
+    }  
+
+    /** Merge two sorted lists */ 
+    public static void merge(int[] list1, int[] list2, int[] temp) {   
+        int current1 = 0; // Current index in list1   
+        int current2 = 0; // Current index in list2  
+        int current3 = 0; // Current index in temp    
+        while (current1 < list1.length && current2 < list2.length) { 
+            if (list1[current1] < list2[current2])       
+                temp[current3++] = list1[current1++];      
+            else        temp[current3++] = list2[current2++];    }   
+        while (current1 < list1.length)      
+            temp[current3++] = list1[current1++];  
+        while (current2 < list2.length)      
+            temp[current3++] = list2[current2++];  
     }
 
     private static int partition(int[] list, int first, int last) 
@@ -110,18 +102,32 @@ public class Sorting
     }  
 
     /** Heap sort method */  
-    public static <E extends Comparable> void heapSort(E[] list) 
-    {    // Create a Heap of integers   
-        Heap<E> heap = new Heap<E>();    // Add elements to the heap    
-        for (int i = 0; i < list.length; i++)      
-            heap.add(list[i]);    // Remove elements from the heap  
-        for (int i = list.length - 1; i >= 0; i--)      
+    public static  int[] heapSort(int []list) 
+    {    //Heap parent at i, left is 2i+1 , right is 2i+2  
+        int [] Return = new int[list.length+1];
+        for(int i = 0; i<list.length/2;i+=3)
+        {
+            Return[i] = list[i];
+            Return[i+1] = list[2*i+1];
+            if(i<list.length/2-2)
+                Return[i+2] = list[2*i+2];
+        }
+        for(int i = 0;i<list.length;i++)
+        {
+            System.out.println(Return[i]);
+        }
+        return Return;
+    } 
+
+    public static <E extends Comparable> void heapSort(E[] list) {    // Create a Heap of integers    
+        Heap<E> heap = new Heap<E>();    // Add elements to the heap   
+        for (int i = 0; i < list.length; i++)     
+            heap.add(list[i]);    // Remove elements from the heap 
+        for (int i = list.length - 1; i >= 0; i--)     
             list[i] = heap.remove();  
     } 
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Random rando = new Random();
         int[] var = new int[20];
         for(int i = 0;i<var.length;i++)
@@ -130,11 +136,67 @@ public class Sorting
             System.out.println(var[i]);
         }
         Sorting sort = new Sorting();
-        sort.quickSort(var);
+        sort.heapSort(var);
         System.out.println("organized list");
         for(int i = 0;i<var.length;i++)
         {
             System.out.println(var[i]);
         }
+    }
+}
+class Heap<E extends Comparable> {  
+    private java.util.ArrayList<E> list = new java.util.ArrayList<E>();  
+    public Heap() {}  
+
+    /** Create a heap from an array of objects */ 
+    public Heap(E[] objects) {   
+        for (int i = 0; i < objects.length; i++)      
+            add(objects[i]);  
+    }  
+
+    /** Add a new object into the heap */ 
+    public void add(E newObject) {    
+        list.add(newObject); // Append to the heap    
+        int currentIndex = list.size() - 1; // The index of the last node   
+        while (currentIndex > 0) {      
+            int parentIndex = (currentIndex - 1) / 2;      // Swap if the current object is greater than its parent   
+            if (list.get(currentIndex).compareTo(list.get(parentIndex)) > 0) 
+            {        E temp = list.get(currentIndex);       
+                list.set(currentIndex, list.get(parentIndex));      
+                list.set(parentIndex, temp);      }     
+            else        
+                break; // the tree is a heap now     
+            currentIndex = parentIndex;    }  }  
+
+    /** Remove the root from the heap */ 
+    public E remove() {   
+        if (list.size() == 0) return null;    
+        E removedObject = list.get(0);    
+        list.set(0, list.get(list.size() - 1));   
+        list.remove(list.size() - 1);  
+        int currentIndex = 0;    
+        while (currentIndex < list.size()) {    
+            int leftChildIndex = 2 * currentIndex + 1;  
+            int rightChildIndex = 2 * currentIndex + 2;      // Find the maximum between two children 
+            if (leftChildIndex >= list.size()) break; // The tree is a heap  
+            int maxIndex = leftChildIndex;     
+            if (rightChildIndex < list.size()) {        
+                if (list.get(maxIndex).compareTo(           
+                    list.get(rightChildIndex)) < 0) {          
+                    maxIndex = rightChildIndex;        }   
+            }
+            // Swap if the current node is less than the maximum   
+            if (list.get(currentIndex).compareTo(list.get(maxIndex)) < 0) {    
+                E temp = list.get(maxIndex);       
+                list.set(maxIndex, list.get(currentIndex));  
+                list.set(currentIndex, temp);       
+                currentIndex = maxIndex;      }     
+            else        
+                break; // The tree is a heap  
+        }    return removedObject;  }  
+
+    /** Get the number of nodes in the tree */ 
+    public int getSize() {    
+        return list.size();
     }
 }
